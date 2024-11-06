381a42cf6aeddcb80587d82693f3cb8b41c438ca
chelsea <chelseazaomi9@gmail.com>
2024-11-06 22:41:34 +0700

    install preline

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,15 @@ export default {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "node_modules/preline/dist/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('preline/plugin'),
+  ],
 }
 
 // /** @type {import('tailwindcss').Config} */
