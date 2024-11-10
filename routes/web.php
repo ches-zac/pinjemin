1c629eda04e48a92bd7bbccc03b80355009b861b
chelsea <chelseazaomi9@gmail.com>
2024-11-10 15:00:52 +0700

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::middleware('auth')->group(function(){
+    Route::get('/', function(){
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::middleware('guest')->group(function(){
+    Route::controller(AuthController::class)->group(function(){
+        Route::name('auth.')->group(function(){
+            Route::get('/login', 'login')->name('login');
+            Route::get('/register', 'register')->name('register');
+        });
+    });
 });
