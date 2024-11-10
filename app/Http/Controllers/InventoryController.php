b7cac97b292794d6e78f3c9cb32129f1611f47cc
Fedra Putri Azzahrah <zaraiyrail@gmail.com>
2024-11-10 18:48:04 +0700

    update: Inventory Controller & Lending Model

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inventory;
+use App\Models\Lending;
 use Illuminate\Http\Request;
+
+use function PHPUnit\Framework\isEmpty;
 
 class InventoryController extends Controller
 {
-    //
+    // Tersedia
+    public function tersedia()
+    {
+        // Menghitung jumlah barang yang dipinjam
+        $data = Inventory::count('kuota')->get();
+        $dipinjam = Lending::count();
+        if ($dipinjam < $data) {return 'Tersedia';} else {return 'Tidak Tersedia';};
+    }
+
+    // Pinjam
+    public function pinjam()
+    {
+
+    }
+
+    // Kembalikan
+    public function kembalikan()
+    {
+
+    }
+
+    // Pengembalian
+    public function pengembalian()
+    {
+        
+    }
 }
--- app/Models/Lending.php
+++ app/Models/Lending.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Lending extends Model
 {
-    //
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'lending_id',
+        'user_id',
+        'inventory_id',
+    ];
 }
