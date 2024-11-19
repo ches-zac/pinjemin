4e909de0de79304af9152e5b2927bf80bda93892
chelsea <chelseazaomi9@gmail.com>
2024-11-19 21:04:36 +0700

    route baru

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Auth\AuthController;
 
 // Auth Routes
 Route::middleware('guest')->group(function () {
-    Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+    Route::get('/', [AuthController::class, 'loginForm'])->name('login');
     Route::post('/login', [AuthController::class, 'login']);
     Route::get('/register', [AuthController::class, 'registerForm'])->name('register');
     Route::post('/register', [AuthController::class, 'register']);
