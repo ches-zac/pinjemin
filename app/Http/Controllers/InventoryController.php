dd4b584da9dc8677f802b93a27760749879d0564
Fedra Putri Azzahrah <zaraiyrail@gmail.com>
2024-11-20 22:47:13 +0700

    update: LendingController function pinjam()

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -21,43 +21,6 @@ class InventoryController extends Controller
         if ($dipinjam < $data) {return 'Tersedia';} else {return 'Tidak Tersedia';};
     }
 
-    // Pinjam
-    public function pinjam(Request $request)
-    {
-        // Cek ketersediaan barang
-        if (!$this->tersedia())
-        {
-            return false; // Atau throw exception
-        }
-
-        // Buat record peminjaman baru
-        $peminjaman = new Lending();
-        $peminjaman->user_id = User::select('nama')->get();
-        $peminjaman->inventory_id = Inventory::select('inventory_id')->get();
-        $peminjaman->tanggal_peminjaman = $request->tanggal_peminjaman;
-        $peminjaman->tanggal_pengembalian = $request->tanggal_pengembalian;
-        $peminjaman->save();
-
-        // return redirect()->route('')->with('success', 'Peminjaman Berhasil')
-
-    }
-
-    // Kembalikan
-    public function kembalikan($id)
-    {
-        // Mengambil data dari tabel Inventory
-        $inventory = Inventory::find($id);
-        // Memeriksa ketersediaan barang
-        if (!$inventory) {
-            throw new \Exception('Barang tidak ditemukan');
-        }
-        // Update status barang
-        $inventory->status = 'tersedia';
-        $inventory->save();
-
-        return redirect()->route('')->with('success', 'Barang berhasil dikembalikan');
-    }
-
     // Pengembalian
     public function pengembalian()
     {
--- app/Http/Controllers/LendingController.php
+++ app/Http/Controllers/LendingController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inventory;
+use App\Models\Lending;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LendingController extends Controller
@@ -12,9 +15,46 @@ class LendingController extends Controller
 
     }
 
+    // Pinjam
+    public function pinjam(Request $request)
+    {
+        // Cek ketersediaan barang, pls ini gaje maaf wkwkwk
+        if (!$this->tersedia())
+        {
+            return false; // Atau throw exception
+        }
+
+        // Buat record peminjaman baru
+        $peminjaman = new Lending();
+        $peminjaman->user_id = User::select('nama')->get();
+        $peminjaman->inventory_id = Inventory::select('inventory_id')->get();
+        $peminjaman->tanggal_peminjaman = $request->tanggal_peminjaman;
+        $peminjaman->tanggal_pengembalian = $request->tanggal_pengembalian;
+        $peminjaman->save();
+
+        // return redirect()->route('')->with('success', 'Peminjaman Berhasil')
+
+    }
+
+    // Kembalikan
+    public function kembalikan($id)
+    {
+        // Mengambil data dari tabel Inventory
+        $inventory = Inventory::find($id);
+        // Memeriksa ketersediaan barang
+        if (!$inventory) {
+            throw new \Exception('Barang tidak ditemukan');
+        }
+        // Update status barang
+        $inventory->status = 'tersedia';
+        $inventory->save();
+
+        return redirect()->route('')->with('success', 'Barang berhasil dikembalikan');
+    }
+
     // Batalkan
     public function batalkan()
     {
-        
+
     }
 }
