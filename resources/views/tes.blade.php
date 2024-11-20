75a61fd1d8b84aa01a54b72f2c90b0eecb173360
mijoo007 <jaehyunmijoo.com>
2024-11-20 22:06:11 +0700

    nambahin vite

--- resources/views/tes.blade.php
+++ resources/views/tes.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Interactive Sidebar</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resource/css/app.css', 'resource/js/app.js'])
 
     <style>
         @keyframes gradientAnimation {
