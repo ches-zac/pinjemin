04bae4bf337f1b9add1049d80d62932574d62a8b
Fedra Putri Azzahrah <zaraiyrail@gmail.com>
2024-11-23 06:57:37 +0700

    update: faq page

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,113 +1,9 @@
-<<<<<<< HEAD
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            FAQ
-        </h2>
-    </x-slot>
-
-    <!-- FAQ -->
-<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-    <!-- Title -->
-    <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-      <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Your questions, answered</h2>
-      <p class="mt-1 text-gray-600 dark:text-neutral-400">Answers to the most frequently asked questions.</p>
-    </div>
-    <!-- End Title -->
-
-    <div class="max-w-2xl mx-auto">
-      <!-- Accordion -->
-      <div class="hs-accordion-group">
-        <div class="hs-accordion hs-accordion-active:bg-gray-100 rounded-xl p-6 dark:hs-accordion-active:bg-white/10 active" id="hs-basic-with-title-and-arrow-stretched-heading-one">
-          <button class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500 focus:outline-none focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400" aria-expanded="true" aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-one">
-            Can I cancel at anytime?
-            <svg class="hs-accordion-active:hidden block shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
-            <svg class="hs-accordion-active:block hidden shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>
-          </button>
-          <div id="hs-basic-with-title-and-arrow-stretched-collapse-one" class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300" role="region" aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-one">
-            <p class="text-gray-800 dark:text-neutral-200">
-              Yes, you can cancel anytime no questions are asked while you cancel but we would highly appreciate if you will give us some feedback.
-            </p>
-          </div>
-        </div>
-
-        <div class="hs-accordion hs-accordion-active:bg-gray-100 rounded-xl p-6 dark:hs-accordion-active:bg-white/10" id="hs-basic-with-title-and-arrow-stretched-heading-two">
-          <button class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500 focus:outline-none focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400" aria-expanded="false" aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-two">
-            My team has credits. How do we use them?
-            <svg class="hs-accordion-active:hidden block shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
-            <svg class="hs-accordion-active:block hidden shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>
-          </button>
-          <div id="hs-basic-with-title-and-arrow-stretched-collapse-two" class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300" role="region" aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-two">
-            <p class="text-gray-800 dark:text-neutral-200">
-              Once your team signs up for a subscription plan. This is where we sit down, grab a cup of coffee and dial in the details.
-            </p>
-          </div>
-        </div>
-
-        <div class="hs-accordion hs-accordion-active:bg-gray-100 rounded-xl p-6 dark:hs-accordion-active:bg-white/10" id="hs-basic-with-title-and-arrow-stretched-heading-three">
-          <button class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500 focus:outline-none focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400" aria-expanded="false" aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-three">
-            How does Preline's pricing work?
-            <svg class="hs-accordion-active:hidden block shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
-            <svg class="hs-accordion-active:block hidden shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>
-          </button>
-          <div id="hs-basic-with-title-and-arrow-stretched-collapse-three" class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300" role="region" aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-three">
-            <p class="text-gray-800 dark:text-neutral-200">
-              Our subscriptions are tiered. Understanding the task at hand and ironing out the wrinkles is key.
-            </p>
-          </div>
-        </div>
-
-        <div class="hs-accordion hs-accordion-active:bg-gray-100 rounded-xl p-6 dark:hs-accordion-active:bg-white/10" id="hs-basic-with-title-and-arrow-stretched-heading-four">
-          <button class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500 focus:outline-none focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400" aria-expanded="false" aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-four">
-            How secure is Preline?
-            <svg class="hs-accordion-active:hidden block shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
-            <svg class="hs-accordion-active:block hidden shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>
-          </button>
-          <div id="hs-basic-with-title-and-arrow-stretched-collapse-four" class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300" role="region" aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-four">
-            <p class="text-gray-800 dark:text-neutral-200">
-              Protecting the data you trust to Preline is our first priority. This part is really crucial in keeping the project in line to completion.
-            </p>
-          </div>
-        </div>
-
-        <div class="hs-accordion hs-accordion-active:bg-gray-100 rounded-xl p-6 dark:hs-accordion-active:bg-white/10" id="hs-basic-with-title-and-arrow-stretched-heading-five">
-          <button class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500 focus:outline-none focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400" aria-expanded="false" aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-five">
-            How do I get access to a theme I purchased?
-            <svg class="hs-accordion-active:hidden block shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
-            <svg class="hs-accordion-active:block hidden shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>
-          </button>
-          <div id="hs-basic-with-title-and-arrow-stretched-collapse-five" class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300" role="region" aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-five">
-            <p class="text-gray-800 dark:text-neutral-200">
-              If you lose the link for a theme you purchased, don't panic! We've got you covered. You can login to your account, tap your avatar in the upper right corner, and tap Purchases. If you didn't create a login or can't remember the information, you can use our handy Redownload page, just remember to use the same email you originally made your purchases with.
-            </p>
-          </div>
-        </div>
-
-        <div class="hs-accordion hs-accordion-active:bg-gray-100 rounded-xl p-6 dark:hs-accordion-active:bg-white/10" id="hs-basic-with-title-and-arrow-stretched-heading-six">
-          <button class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500 focus:outline-none focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400" aria-expanded="false" aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-six">
-            Upgrade License Type
-            <svg class="hs-accordion-active:hidden block shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
-            <svg class="hs-accordion-active:block hidden shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>
-          </button>
-          <div id="hs-basic-with-title-and-arrow-stretched-collapse-six" class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300" role="region" aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-six">
-            <p class="text-gray-800 dark:text-neutral-200">
-              There may be times when you need to upgrade your license from the original type you purchased and we have a solution that ensures you can apply your original purchase cost to the new license purchase.
-            </p>
-          </div>
-        </div>
-      </div>
-      <!-- End Accordion -->
-    </div>
-  </div>
-  <!-- End FAQ -->
-</x-app-layout>
-=======
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Interactive Sidebar</title>
+    <title>Frequently Asked Questions</title>
     <script src="https://cdn.tailwindcss.com"></script>
     @vite(['resource/css/app.css', 'resource/js/app.js'])
 
@@ -132,7 +28,7 @@
 <body class="bg-gray-100">
     <!--Header-->
     <header class="gradient-animation text-white text-center text-2xl font-bold py-4 px-8 sticky top-0 z-50">
-        Inventory:ITEM
+        Frequently Asked Questions
     </header>
     <div class="flex h-screen">
         <!-- Sidebar -->
@@ -181,54 +77,57 @@
 
 
         <!-- Content -->
-        <div class="container mx-auto px-4">
-            <h2 class="text-2xl font-bold mb-4">Frequently Asked Questions</h2>
-            <div class="accordion">
-              <div class="accordion-item bg-white border border-gray-200">
-                <h2 class="accordion-header mb-0" id="headingOne">
-                  <button class="accordion-button relative flex items-center 1  p-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M19 9l-7 7-7-7" />
-                    </svg>
-                    Pertanyaan 1
-                  </button>
-                  <p>              Ini adalah jawaban untuk pertanyaan pertama. Anda dapat menambahkan lebih banyak teks di sini.</p>
-                  <p>              Anda juga  bisa menambahkan elemen HTML lainnya seperti gambar atau daftar.</p>
-                </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                  <div class="accordion-body p-5">
-                    Jawaban untuk pertanyaan 1.
-                    <p>Ini adalah jawaban untuk pertanyaan pertama. Anda dapat menambahkan lebih banyak teks di sini.</p>
-                    <p>Anda juga bisa menambahkan elemen HTML lainnya seperti    tau daftar.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div class="flex flex-row">
 
-
-          <div class="container mx-auto px-4">
-            <h2 class="text-2xl font-bold mb-4">Frequently Asked Questions</h2>
-            <div class="accordion">
-              <div class="accordion-item bg-white border border-gray-200">
-                <h2 class="accordion-header mb-0" id="headingOne">
-                  <button class="accordion-button relative flex items-center 1  p-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M19 9l-7 7-7-7" />
-                    </svg>
-                    Pertanyaan 1
-                  </button>
-                  <p>              Ini adalah jawaban untuk pertanyaan pertama. Anda dapat menambahkan lebih banyak teks di sini.</p>
-                  <p>              Anda juga  bisa menambahkan elemen HTML lainnya seperti gambar atau daftar.</p>
-                </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                  <div class="accordion-body p-5">
-                    Jawaban untuk pertanyaan 1.
-                  </div>
+            <div class="container mx-auto px-4 mt-4">
+                <h2 class="text-2xl font-bold mb-4 mt-3">Frequently Asked Questions</h2>
+                <div class="accordion">
+                <div class="accordion-item max-h-fit rounded-xl bg-white border border-gray-200">
+                    <h2 class="accordion-header mb-0" id="headingOne">
+                    <button class="accordion-button relative flex items-center 1  p-5 mt-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M19 9l-7 7-7-7" />
+                        </svg>
+                        Pertanyaan 1
+                    </button>
+                    <p class="px-5 py-3">Ini adalah jawaban untuk pertanyaan pertama. Anda dapat menambahkan lebih banyak teks di sini.</p>
+                    <p class="px-5 py-3">Anda juga  bisa menambahkan elemen HTML lainnya seperti gambar atau daftar.</p>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div class="accordion-body p-5">
+                        Jawaban untuk pertanyaan 1.
+                        <p class="px-3 py-2">Ini adalah jawaban untuk pertanyaan pertama. Anda dapat menambahkan lebih banyak teks di sini.</p>
+                        <p class="px-3 py-2">Anda juga bisa menambahkan elemen HTML lainnya seperti    tau daftar.</p>
+                    </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
+
+            <div class="container mx-auto px-4 mt-4">
+                <h2 class="text-2xl font-bold mb-4 mt-3">Frequently Asked Questions</h2>
+                <div class="accordion">
+                    <div class="accordion-item max-h-fit rounded-xl bg-white border border-gray-200">
+                        <h2 class="accordion-header mb-0" id="headingOne">
+                        <button class="accordion-button relative flex items-center 1  p-5 mt-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24">
+                                <path d="M19 9l-7 7-7-7" />
+                            </svg>
+                            Pertanyaan 1
+                        </button>
+                        <p class="px-5 py-3">Ini adalah jawaban untuk pertanyaan pertama. Anda dapat menambahkan lebih banyak teks di sini.</p>
+                        <p class="px-5 py-3">Anda juga  bisa menambahkan elemen HTML lainnya seperti gambar atau daftar.</p>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body p-5">
+                            Jawaban untuk pertanyaan 1.
+                            <p class="px-3 py-2">Ini adalah jawaban untuk pertanyaan pertama. Anda dapat menambahkan lebih banyak teks di sini.</p>
+                            <p class="px-3 py-2">Anda juga bisa menambahkan elemen HTML lainnya seperti    tau daftar.</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 
@@ -345,4 +244,3 @@
 
 </body>
 </html>
->>>>>>> 7f3df91e95398d34f652820bb9887d5bc6f08714
