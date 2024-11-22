225d916103188bddf8d70ef5d3248773a4e9f9e0
mijoo007 <jaehyunmijoo.com>
2024-11-23 00:02:14 +0700

    ADMIN DONE

--- resources/views/admin/dashborad.blade.php
+++ resources/views/admin/dashborad.blade.php
@@ -28,7 +28,7 @@
 <body class="bg-gray-100">
     <!--Header-->
     <header class="gradient-animation text-white text-center text-2xl font-bold py-4 px-8 sticky top-0 z-50">
-        Data Pengguna - ADMIN
+        Dashboard
     </header>
     <div class="flex h-screen">
         <!-- Sidebar -->
@@ -85,7 +85,7 @@
                         <th class="border border-gray-200 py-2">Barang</th>
                         <th class="border border-gray-200 py-2">Ruangan</th>
                         <th class="border border-gray-200 py-2">Pengembalian</th>
-                        <th class="border border-gray-200 py-2">Aksi</th>
+                        <th class="border border-gray-200 py-2">Status</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -94,12 +94,12 @@
                         <td class="border border-gray-200 py-2 text-center">Data 2</td>
                         <td class="border border-gray-200 py-2 text-center">Data 3</td>
                         <td class="border border-gray-200 py-2 text-center">Data 4</td>
-                        <td class="border border-gray-200 py-2 text-center">Data 4</td>
                         <td class="border border-gray-200 py-2 text-center">
                             <button class="text-black font-bold py-2 px-4 rounded">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                    <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+                                  </svg>
+
                         </td>
                     </tr>
                     <tr>
@@ -107,12 +107,23 @@
                         <td class="border border-gray-200 py-2 text-center">Data 6</td>
                         <td class="border border-gray-200 py-2 text-center">Data 7</td>
                         <td class="border border-gray-200 py-2 text-center">Data 8</td>
-                        <td class="border border-gray-200 py-2 text-center">Data 4</td>
                         <td class="border border-gray-200 py-2 text-center">
                             <button class="text-black font-bold py-2 px-4 rounded">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                    <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clip-rule="evenodd" />
+                                  </svg>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="border border-gray-200 py-2 text-center">Data 5</td>
+                        <td class="border border-gray-200 py-2 text-center">Data 6</td>
+                        <td class="border border-gray-200 py-2 text-center">Data 7</td>
+                        <td class="border border-gray-200 py-2 text-center">Data 8</td>
+                        <td class="border border-gray-200 py-2 text-center">
+                            <button class="text-black font-bold py-2 px-4 rounded">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                    <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clip-rule="evenodd" />
+                                  </svg>
                         </td>
                     </tr>
                 </tbody>
