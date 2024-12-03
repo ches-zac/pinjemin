29131a3d87177443fd3604ebe058cf782511eb72
chelsea <chelseazaomi9@gmail.com>
2024-12-03 19:08:31 +0700

    rapihin edit profile sama tombol tambah kategori

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->app['router']->aliasMiddleware('admin', Admin::class);
+        \Carbon\Carbon::setLocale('id');
     }
 }
--- resources/views/admin/category/show.blade.php
+++ resources/views/admin/category/show.blade.php
@@ -41,13 +41,13 @@
                 </tbody>
             </table>
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
-                + TAMBAH PENGGUNA
+                + TAMBAH Kategori
             </button>
         </div>
     </div>
 @endsection
 
-{{-- 
+{{--
     <script>
         // Fungsi untuk mengambil data pengguna dari backend
         async function fetchData() {
