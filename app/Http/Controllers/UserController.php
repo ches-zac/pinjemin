d23fab2f872554bee278ebd560018373e250fb2e
chelsea <chelseazaomi9@gmail.com>
2024-12-03 20:07:48 +0700

    edit user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,14 +9,38 @@ class UserController extends Controller
 {
     public function show() {
         $users = User::all();
-        $title = 'user';
+        $title = 'Daftar User';
         return view('admin.users_manage.show', compact('users', 'title'));
     }
 
     public function edit(User $user)
     {
-        $title = 'edit-user';
-        return view('admin.users_manage.show', compact('user', 'title'));
+        $title = 'Edit User';
+        return view('admin.users_manage.edit', compact('user', 'title'));
+    }
+
+    public function update(Request $request)
+    {
+        // Validasi input
+        $validatedData = $request->validate([
+            'role' => 'required|in:user,admin', // Hanya menerima nilai 'user' atau 'admin'
+        ]);
+
+        // Ambil user berdasarkan ID yang dikirimkan
+        $user = User::findOrFail($request->user_id); // Pastikan `user_id` dikirim dari form
+
+        // Perbarui role user
+        $user->role = $validatedData['role'];
+        $user->save(); // Simpan perubahan ke database
+
+        // Redirect dengan pesan sukses
+        return redirect()->route('show.users')->with('success', 'Role pengguna berhasil diperbarui.');
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete(); // Hapus data
+        return redirect()->route('show.users')->with('success', 'Barang berhasil dihapus.');
     }
 
 }
--- resources/views/admin/category/show.blade.php
+++ resources/views/admin/category/show.blade.php
@@ -8,9 +8,7 @@
                     <tr class="bg-teal-300">
                         <th class="border border-gray-200 py-2">No</th>
                         <th class="border border-gray-200 py-2">Barang</th>
-                        <th class="border border-gray-200 py-2">Kategori</th>
-                        <th class="border border-gray-200 py-2">Kuota</th>
-                        <th class="border border-gray-200 py-2">Aksi</th>
+
                     </tr>
                 </thead>
                 <tbody>
