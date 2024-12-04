4107d4294228187dc6f8ed2a1924255584a9f49a
ches-zac <63915333+ches-zac@users.noreply.github.com>
2024-12-04 08:30:10 +0700

    Update item.blade.php

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-        <!-- Content -->
-      @extends('layouts.app')
-
-@section('content')
 <div class="container mx-auto px-4 py-8">
     <!-- Grid Responsif -->
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
